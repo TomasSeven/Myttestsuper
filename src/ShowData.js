4e8d94ccfd8956c3c4931bd328045acdb029a938
TomasSeven <30586945+TomasSeven@users.noreply.github.com>
2018-03-20 22:33:20 +0100

    new commit

--- src/ShowData.js
+++ src/ShowData.js
@@ -6,17 +6,56 @@ class Fetch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      information: []
+      information: [],
     };
   }
   render() {
+    console.log("testar");
     console.log(this.props.url);
-    fetch(this.props.url).then(results => {
-      console.log(results.json);
-      return results.json();
-    } );
+    //fetch(this.props.url).then(results => {
+    //  console.log(results);
+     // return results.json();
+        fetch(
+          "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=1min&apikey=xxx"
+        )
+          .then(function (response) {
+            if (response.status !== 200) {
+              console.log(
+                "Looks like there was a problem. Status Code: " + response.status
+              );
+              return;
+            }
+
+            // Examine the text in the response
+            response.json().then(function (data) {
+              console.log(data)
+              return response.json();
+            });
+
+            // deliver data
+
+
+          })
+          .catch(function (err) {
+            console.log("Fetch Error :-S", err);
+          }) 
+       //https://blog.hellojs.org/fetching-api-data-with-react-js-460fe8bbf8f2
+          .then(data => {
+            let information = data.response.map((info) => {
+              return (
+                <div>{info.response}>
+                </div>
+              )
+            })
+            this.setState(information: information);
+            console.log("state",this.state.information);
+          }
+          
+          )
+          ;
+
 return(
-<div>Test</div>
+  <div>Test</div>
 
 );
 
@@ -26,7 +65,7 @@ return(
 class Api extends React.Component {
   render() {
     return (
-      <Fetch url="https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=1min&apikey=xx">
+      <Fetch url="https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=1min&apikey=xxx">
        // <ApiResponse />
       </Fetch>
     )
